a6b6e7759719dc5da068cd343731bef09412f041
Gabor Foldes <gabor.foldes@gmail.com>
2014-01-19 01:22:46 +0000

    Site update

--- controller-pi/controller.cpp
+++ controller-pi/controller.cpp
@@ -13,7 +13,7 @@ RF24 radio("/dev/spidev0.0", 8000000, 25);
 
 // Radio pipe addresses for the 2 nodes to communicate
 const uint64_t pipes[3] = { 0xF0F0F0F0D2LL, 0xF0F0F0F0E1LL, 0xF0F0F0F0E2LL };
-const char pipe_name[3][64] = { "Base", "Living room", "Portable" };
+const char pipe_name[3][64] = { "Base", "Bassinet", "Portable" };
 
 // log file
 ofstream logfile;
--- site/app.js
+++ site/app.js
@@ -1,19 +1,33 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var sys = require('sys');
+var exec = require('child_process').exec;
 
 app.engine('hamlc', require('haml-coffee').__express);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
+//  res.send(200);
   fs.readFile('/data/test_temp', 'utf-8', function(err, data) {
     if (err) throw err;
-
     var lines = data.trim().split('\n');
     var lastLine = lines.slice(-1);
     res.render('index.hamlc', JSON.parse(lastLine));
   });
 });
 
-app.listen(8080);
+app.get('/xmas/:state', function(req, res){
+  var onoff = (req.params.state === "on") ? "on" : "off";
+  var child = exec("/home/pi/code/homeapp/controller-pi/light " + onoff, function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+    res.send(200);
+  })
+});
+
+app.listen(80);
 console.log('Listening on port 80');
 
