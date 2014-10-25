d389e94b6746765222ece0ca5f819afff82ccdb3
Gabor Foldes <gabor.foldes@gmail.com>
2014-10-25 01:57:13 -0700

    using firebase as data store

--- controller-pi/controller.js
+++ controller-pi/controller.js
@@ -20,24 +20,29 @@ radio.autoRetransmit({
 
 // Set up data store
 
-var Datastore = require('nedb')
-  , db = new Datastore({ filename: '/data/homeapp.db', autoload: true });
+//var Datastore = require('nedb')
+//  , db = new Datastore({ filename: '/data/homeapp.db', autoload: true });
+
+var Firebase = require('firebase');
+var db = new Firebase('https://vivid-fire-6554.firebaseio.com/');
 
 
 // Handlers
 
 function process_signal (d) {
+	device = this._addr.toString('hex');
 	c = d.readUInt32BE(0)/100;
 	signal = { timestamp: new Date().toJSON(),
-		   device: {
-		     id: this._addr.toString('hex')
-		   },
+//		   device: {
+//		     id: this._addr.toString('hex'),
+//		     name: this._name
+//		   },
 		   data: {
 		     celsius: c,
 		     fahrenheit: (c * 9 / 5 ) + 32
 		   }
 		 };
-	db.insert(signal);
+	db.child('devices/' + device + '/signals').push(signal);
 }
 
 function error_handler (e) {
@@ -53,6 +58,9 @@ radio.begin(function() {
 
 	var rx1 = radio.openPipe('rx', 0xF0F0F0F0E1); // Listen at address 
 	var rx3 = radio.openPipe('rx', 0xF0F0F0F0E3); // Listen at address 
+
+	db.child('devices/' + rx1._addr.toString('hex') + '/name').set("Kiki's room");
+	db.child('devices/' + rx3._addr.toString('hex') + '/name').set("Living room");
 
 	rx1.on('data', process_signal).on('error', error_handler);
 	rx3.on('data', process_signal).on('error', error_handler);
