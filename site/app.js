4442fd3882ad7a61e3727a65b002d94a84d5f4d6
Gabor Foldes <gabor.foldes@gmail.com>
2014-10-25 22:58:45 -0700

    updating front-end to use firebase

--- site/app.js
+++ site/app.js
@@ -4,8 +4,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var Datastore = require('nedb')
-  , db = new Datastore({ filename: '/data/homeapp.db', autoload: true });
+// var Datastore = require('nedb')
+//   , db = new Datastore({ filename: '/data/homeapp.db', autoload: true });
+
+var Firebase = require('firebase');
+var db = new Firebase('https://vivid-fire-6554.firebaseio.com/');
 
 var port = process.argv[2] || 3080;
 
@@ -18,9 +21,17 @@ app.get('/', function(req, res) {
 });
 
 app.get('/devices/:id', function(req, res) {
-  db.find({ "device.id": "f0f0f0f0e" + req.params.id }).sort({ timestamp: -1 }).limit(1440).exec(function (err, data) {
-    res.render('device.html.ejs', { signals: data });
+
+  db.child('devices/f0f0f0f0e' + req.params.id).once('value', function (data) {
+  	console.log('GET /devices/' + req.params.id );
+    res.render('device.html.ejs', { device: data.val() });
+  }, function (e) {
+    console.log('The read failed: ' + e.code);
   });
+
+  // db.find({ "device.id": "f0f0f0f0e" + req.params.id }).sort({ timestamp: -1 }).limit(1440).exec(function (err, data) {
+  //   res.render('device.html.ejs', { signals: data });
+  // });
 });
 
 
