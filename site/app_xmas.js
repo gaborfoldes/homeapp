fc1ea256dd32abf6e14ac18d1ef0320a8ad69d07
Gabor Foldes <gabor.foldes@gmail.com>
2015-12-24 23:52:36 -0800

    xmas tree switch for the kiki

--- site/app_xmas.js
+++ site/app_xmas.js
@@ -4,52 +4,24 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-// var Datastore = require('nedb')
-//   , db = new Datastore({ filename: '/data/homeapp.db', autoload: true });
-
-var Firebase = require('firebase');
-var db = new Firebase('https://vivid-fire-6554.firebaseio.com/');
-
 var port = process.argv[2] || 3080;
 
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  //res.redirect('/dashboard')
   res.status(200).end();
 });
 
-app.get('/devices/:id', function(req, res) {
-
-  res.render('device.html.ejs', { device: {name: 'Test', id: 'f0f0f0f0e' + req.params.id } });
-
-
-//  db.child('devices/f0f0f0f0e' + req.params.id + '/signals').startAt(Date.now()-86400000).once('value', function (data) {
-//  	console.log('GET /devices/' + req.params.id );
-//    res.render('device.html.ejs', { device: {name: 'Test', id: 'f0f0f0f0e' + req.params.id } });
-//  }, function (e) {
-//    console.log('The read failed: ' + e.code);
-//  });
-
-  // db.find({ "device.id": "f0f0f0f0e" + req.params.id }).sort({ timestamp: -1 }).limit(1440).exec(function (err, data) {
-  //   res.render('device.html.ejs', { signals: data });
-  // });
-});
-
-
 app.get('/xmas', function(req, res) {
-  res.render('xmas.html.ejs');
+  res.render('xmas2015.html.ejs');
 });
-
 
 app.get('/xmas/on', function(req, res) {
-    db.child('devices/f0f0f0f0d2/xmas').set(1);
     res.status(200).end();
 });
 
 app.get('/xmas/off', function(req, res) {
-    db.child('devices/f0f0f0f0d2/xmas').set(0);
     res.status(200).end();
 });
 
